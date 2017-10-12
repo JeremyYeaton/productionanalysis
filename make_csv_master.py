a447d8cbac12f6d8dab7a6a2e33bbb09a34701a5
Jeremy Yeaton <jdyeaton27@gmail.com>
2017-10-12 15:19:23 +0200

    created maxmin and metadata summary files

--- make_csv_master.py
+++ make_csv_master.py
@@ -48,6 +48,7 @@ for file in files:
 		f.close()
 
 csvlines = []
+metalines = []
 # Calculate descriptive stats
 for s in subjects:
 	sublines = []
@@ -56,23 +57,65 @@ for s in subjects:
 	for line in lines:
 		if line[1] == s:
 			sublines.append(line)
+# Define set of lines for subject
 	data[s] = [sublines]
 	for line in sublines:
 		keyval = float(line[5])
 		if line[1] == s:
 			total_f0 += keyval
+#calculate subject mean f0
 	subj_mean = total_f0/len(sublines)
 	for line in sublines:
-		sum_of_squares += (keyval-subj_mean)**2
+		demeaned_keyval = keyval-subj_mean
+		sum_of_squares += (demeaned_keyval)**2
 	subj_var = sum_of_squares/(len(sublines)-1)
 	subj_stdev = subj_var**0.5
+#append to data dictionary
 	data[s].append([subj_mean,subj_var,subj_stdev])
 	for line in sublines:
-		line.append(",".join([str(subj_mean),str(subj_var),"%s"%str(keyval/subj_mean),"%s\n"%str(subj_stdev)]))
+		demeaned_keyval = keyval-subj_mean
+		line.append(",".join([str(demeaned_keyval),str(subj_mean),str(subj_var),"%s"%str(demeaned_keyval/subj_stdev),"%s\n"%str(subj_stdev)]))
 		csvlines.append(line)
+	metalines.append([s,str(subj_mean),str(subj_var),"%s\n"%str(subj_stdev)])
 
-f = open("master.csv",'w')
-f.write(",".join(["obj_id","subj","trial","syll","series","raw_f0","condition","mean","var","weight_dist","stdev\n"]))
+m = open("rproj/data/subj_meta.csv","w")
+m.write(",".join(["subj","subj_mean","subj_var","subj_stdev\n"]))
+for line in metalines:
+	m.write(",".join(line))
+m.close()
+
+f = open("rproj/data/master.csv",'w')
+f.write(",".join(["obj_id","subj","trial","syll","series","raw_f0","condition","demeaned_f0","mean","var","weight_dist","stdev\n"]))
 for line in csvlines:
 	f.write(",".join(line))
 f.close()
+
+xlines = []
+for file in files:
+	if file.endswith(".means"):
+		f = open(os.path.realpath(p+"/"+file))
+		c = csv.reader(f, delimiter = "\t")
+		firstline = 2
+		aimecheck = True
+		linenum = 5
+		for key in condict:
+			if int(file[4:6]) > condict[key][0] and int(file[4:6]) < condict[key][1]:
+				condition = key
+		for line in c:
+			if firstline:
+				firstline -= 1
+			elif not line[0] == "_":
+				if aimecheck and line[0] == "nEm":
+					aimecheck = False
+					linenum += 10
+				line.append(linenum)
+				nline = [file[:6],str(file[:3]),file[4:6],str(line[0]),str(line[10]),str(line[1]),str(line[2]),str(line[7]),"%s\n"%condition]
+				xlines.append(nline) 
+				linenum += 10
+		f.close()
+
+x = open("rproj/data/max_min_syll.csv","w")
+x.write(",".join(["obj_id","subj","trial","syll","series","max_f0","min_f0","duration","condition\n"]))
+for line in xlines:
+	x.write(",".join(line))
+x.close()
--- rproj/scripts/data_cleaner.R
+++ rproj/scripts/data_cleaner.R
@@ -6,7 +6,18 @@ library(purrr)
 # Full data on election results
 f0_over_time = read.table("data/master.csv", header=T, sep=",")
 
+line_max_min = read.table("data/max_min_syll.csv",header=T,sep=",")
+
+subj_meta_data = read.table("data/subj_meta.csv",header = T, sep = ",")
+
 xtabs(~condition,f0_over_time)
+
+aggregate(f0_over_time[,-c(1,3:5,7)], by = list(f0_over_time$subj),
+          sd, na.rm = TRUE)
+
+aggregate(f0_over_time[,-c(1,3:5,7)], by = list(f0_over_time$subj),
+          mean, na.rm = TRUE)
+
 
 f0_over_time_clean = f0_over_time %>% 
   filter(series < 60)
